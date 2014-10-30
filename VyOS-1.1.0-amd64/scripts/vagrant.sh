2a5f7e3bf1dba52b42e27ffc6aca1d62d7a236a5
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2014-10-31 02:22:12 +0900

    Fix typo

--- VyOS-1.1.0-amd64/scripts/vagrant.sh
+++ VyOS-1.1.0-amd64/scripts/vagrant.sh
@@ -3,12 +3,6 @@ source /opt/vyatta/etc/functions/script-template
 # Set up Vagrant.
 date | sudo tee /etc/vagrant_box_build_time
 
-# Create the user vagrant with password vagrant
-set system login user vagrant authentication plaintext-password vagrant
-set system login user vagrant level admin
-commit
-save
-
 # Install vagrant keys
 PUBLIC_KEY=$(curl -s 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub')
 TYPE=$(echo $PUBLIC_KEY | awk '{print $1}')
